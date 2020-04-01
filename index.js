89bbf037b07dee38715b94dd7983d485f01657fc
Fira Donin <fsdonin@gmail.com>
2020-04-01 02:52:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // add solution here
-function theBeatlesPlay(musicians, instruments)
+function theBeatlesPlay(musicians, instruments) {
+  var newArr = []
+  for (var i=0; i< musicians.length; i++) {
+    let tempString = `${musicians[i]} plays ${instruments[i]}.`
+    newArr.push(tempString)
+  }
+  return newArr
+}
